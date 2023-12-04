a60a0b518c3b80b0b2e587bb378a10ef96cc7fbf
Paddasravani-05 <145531329+Paddasravani-05@users.noreply.github.com>
2023-12-04 19:15:07 +0530

    Add files via upload

---
+++ TO_DO_LIST.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import*
+
+root=Tk()
+root.title("TO_DO_LIST")
+root.geometry("400x700+400+100")
+root.resizable(False,False)
+
+
+task_list=[]
+
+Image_icon=PhotoImage(file="https://pluspng.com/img-png/to-do-list-png-the-power-of-a-to-do-list-imodelafrica-1024.png")
+root.iconphoto(False,
+
+
+root.mainloop()
